0ae98288779e4fd7a1644aa06784e62f58d619e0
tvibhu12 <tvibhu12@gmail.com>
2019-02-11 22:36:51 +0530

    add a new test \'feed\'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,33 +27,53 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /*a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
-
+it('Url are defined',function(){
+    for(var i = 0;i < allFeeds.length;i++)
+    {
+        expect(allFeeds[i].url).toBeDefined();
+        expect(allFeeds[i].url.length).not.toBe(0);
+    }
+   
+});
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('Name are defined',function(){
+            for(var i = 0;i < allFeeds.length;i++)
+            {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
     });
-
+});
 
     /* TODO: Write a new test suite named "The menu" */
 
+    describe('The menu',function(){
+
+
+    
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+it('menu element is hidden',function(){
 
+    
+});
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+        });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
@@ -69,4 +89,4 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-}());
+});
